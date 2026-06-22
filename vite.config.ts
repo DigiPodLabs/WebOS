2591a8689f8fc09b6c824423a1e70242959c838d
DigiPod Chairman <parwin889@gmail.com>
2026-06-22 13:39:44 +0800

    Use custom domain

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
-import path from "path"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
-import { inspectAttr } from 'plugin-inspect-react-code'
+import path from "path";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import { inspectAttr } from "plugin-inspect-react-code";
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/WebOS/',
+  base: "/",
   plugins: [inspectAttr(), react()],
   server: {
     port: 3000,
